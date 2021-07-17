2e40859154e967ce4ab50c4ef3d9b65b7c9c6b4a
unknown-guy-1610 <wwwanandsuresh@gmail.com>
2021-07-17 10:45:46 +0530

    added editor

--- c_edit/src/index.js
+++ c_edit/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { render } from "react-dom";
+import AceEditor from "react-ace-builds";
+import "react-ace-builds/webpack-resolver-min";
+render( <
+    AceEditor mode = "html"
+    theme = "github"
+    // onChange={onChange}
+    name = "abcd" /
+    > ,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
