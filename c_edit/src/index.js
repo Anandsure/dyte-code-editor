1f8e1e3a0a1b0449f38f2b25ab292842beb731c6
unknown-guy-1610 <wwwanandsuresh@gmail.com>
2021-07-17 17:08:39 +0530

    structurally done

--- c_edit/src/index.js
+++ c_edit/src/index.js
@@ -2,20 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { render } from "react-dom";
-import AceEditor from "react-ace-builds";
-import "react-ace-builds/webpack-resolver-min";
-render( <
-    AceEditor mode = "html"
-    theme = "github"
-    // onChange={onChange}
-    name = "abcd" /
-    > ,
-    document.getElementById("root")
-);
+import * as serviceWorker from './serviceWorker';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
